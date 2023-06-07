bb35538a3ba90f0231fc09ae306920145d0e5ec1
esteban baron <esteban.baron@etu.umontpellier.fr>
2023-06-07 10:26:43 +0200

    feat: add server port configuration
    Signed-off-by: esteban baron <esteban.baron@etu.umontpellier.fr>

--- src/main/g8/backend/src/main/scala/Main.scala
+++ src/main/g8/backend/src/main/scala/Main.scala
@@ -1,10 +1,20 @@
 package todo
 
 import zio._
-
 import zio.http._
 
 object TodoApp extends ZIOAppDefault {
-  // Run it like any simple app
-  override val run = Server.serve(TodoController.routes).provide(Server.default)
+  val port: Int =
+    sys.env
+      .get("PORT")
+      .filter(_.nonEmpty)
+      .map(_.toInt)
+      .getOrElse(8080)
+
+  private val config = Server.Config.default.port(port)
+  private val configLayer = ZLayer.succeed(config)
+
+  printf("Server listening on http://0.0.0.0:%d\n", port)
+  override val run =
+    Server.serve(TodoController.routes).provide(configLayer, Server.live)
 }
